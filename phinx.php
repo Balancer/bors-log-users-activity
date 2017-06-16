6955a5dcfaa48e358cdaaf5212b6b2547f8e2819
Balancer <balancer@balancer.ru>
2017-06-16 17:16:42 +0300

    name fix

--- phinx.php
+++ phinx.php
@@ -8,9 +8,9 @@ return [
 	],
 
 	'environments' => [
-		'default_database' => '	changeable',
+		'default_database' => 'changeable',
 		'default_migration_table' => 'phinxlog_b2_users_activity',
-		'	changeable' => [
+		'changeable' => [
 			'name' => \B2\Cfg::get('b2.changeable.db'),
 			'connection' => driver_mysql::factory(\B2\Cfg::get('b2.changeable.db'))->connection(),
 		]
